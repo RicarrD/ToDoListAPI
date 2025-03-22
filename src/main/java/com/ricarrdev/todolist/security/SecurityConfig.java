271e64b5327c84d420b4b902994deac3dd36b430
Ricarrdev <ricarrdev@gmail.com>
2025-03-22 13:39:29 -0300

    feat: start the implementation of security and OAuth

---
+++ src/main/java/com/ricarrdev/todolist/security/SecurityConfig.java
@@ -0,0 +1,18 @@
+//package com.ricarrdev.todolist.security;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    SecurityFilterChain securityFilterChain(HttpSecurity http) {
+//
+//        return http.authorizeHttpRequests(auth -> {
+//            auth.requestMatchers()
+//        })
+//    }
+//}
